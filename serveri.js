3f47fc407021bce392c30f3d6ca432c663631b30
Teemu Luhtanen <teemu.luhtanen@gmail.com>
2020-12-03 11:25:05 +0200

    css muutoksia

--- serveri.js
+++ serveri.js
@@ -63,8 +63,9 @@ var maara;
 app.get('/public/varausvahvistus.html', function (req, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(varausvahvistus);
-    response.write('<p id="etusivup">Kohde:'+kohde+ ' lähtopäivä:' + lahtoaika+ ' Varattuja paikkoja:' + maara+'<p>');
-
+    response.write('<p id="vahvistustekstit">Kohde: ' + kohde +'<p>');
+    response.write('<p id="vahvistustekstit">Lähtopäivä: ' + lahtoaika +'<p>');
+    response.write('<p id="vahvistustekstit">Varattuja paikkoja: ' + maara +'<p>');
     //res.sendFile(path.join(__dirname + '/public/varausvahvistus.html'));
     console.log("Varausvahvistus ladattu!");
 
